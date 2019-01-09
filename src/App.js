9ab5131002713c414fda5ac122aa5700ec3c1f5c
ibushah <username.ibu@gmail.com>
2019-01-09 18:25:11 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -2,27 +2,142 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+  // CHART
+  chart = [
+    {
+      id: 1,
+      peepsID: '1',
+      bookLikedID: '1',
+    },
+    {
+      id: 2,
+      peepsID: '2',
+      bookLikedID: '1',
+    },
+    {
+      id: 3,
+      peepsID: '4',
+      bookLikedID: '5',
+    },
+    {
+      id: 4,
+      peepsID: '5',
+      bookLikedID: '2',
+    },
+    {
+      id: 5,
+      peepsID: '3',
+      bookLikedID: '5',
+    },
+    {
+      id: 6,
+      peepsID: '6',
+      bookLikedID: '4',
+    },
+  ];
+
+  //   PEEEPS
+  peeps = {
+    1: {
+      id: 1,
+      name: 'Fazal Deen',
+      readerCategory: 'champ',
+    },
+    2: {
+      id: 2,
+      name: 'Irfan',
+      readerCategory: 'rising-star',
+    },
+    3: {
+      id: 3,
+      name: 'Muneeb',
+      readerCategory: 'beginner',
+    },
+    4: {
+      id: 4,
+      name: 'Osama',
+      readerCategory: 'champ',
+    },
+    5: {
+      id: 5,
+      name: 'Najam',
+      readerCategory: 'champ',
+    },
+    6: {
+      id: 6,
+      name: 'Aamir',
+      readerCategory: 'beginner',
+    },
+  };
+
+  //   BOOKS
+
+  books = {
+    1: {
+      id: 1,
+      name: 'Imran Series',
+    },
+    2: {
+      id: 2,
+      name: 'Harry Potter',
+    },
+    3: {
+      id: 3,
+      name: 'I Am Malala',
+    },
+    4: {
+      id: 4,
+      name: 'Bang-e-Dara by Allama Iqbal',
+    },
+    5: {
+      id: 5,
+      name: 'Jokes 101',
+    },
+  };
+
+
+  // print function
+
+  print() {
+    var ab = []
+    this.chart.map((v, i) => {
+
+     
+      for(var k in this.peeps)
+      {
+        if(v.peepsID==this.peeps[k].id)
+        {
+      ab[i]=this.peeps[k].name;
+      
+        }
+        
+      }
+      for(var kk in this.books)
+      {
+       
+        if(v.bookLikedID==this.books[kk].id)
+        {
+          
+          ab[i]+=" likes a "+this.books[kk].name;
+        }
+      }
+    })
+          return ab;
+      }
+
+render()
+{
+  
+
+  return (
+    <ul>
+      {this.print().map((v,i) => <li key={i}>{v}</li>)}
+    </ul>
+
+  )
+}
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+let id=document.getElementById('root');
+
+
+
+ReactDOM.render(<App />,id);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
